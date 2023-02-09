086e95efa07146e520a257920798f30f40541631
Isabelle  Bicudo <87657255+IsabelleBic20@users.noreply.github.com>
2023-02-08 21:37:02 -0400

    Add files via upload
    O professor Rafael adora computação gráfica. Ele gosta muito de renderizar objetos, especialmente bonecas. Mas ele não gosta de desenhar polígonos regulares usando triângulos. Fazer isso consiste em compor um polígono regular usando apenas triângulos, de forma que os triângulos não se interceptem e se utilize o menor número possível de triângulos na composição. Veja um quadrado e um pentágono com os triângulos na figura.
    
    Poligonos
    
    O professor Rafael pediu para você calcular qual o número mínimo de triângulos necessários para compor um polígono regular de N lados.
    
    Entrada
    A entrada é composta por um número N (3 ≤ N ≤ 109), que indica o número de lados de um polígono regular.
    
    Saída
    A saída é o número mínimo de triângulos necessários para compor um polígono regular de N lados.

---
+++ 2058 Triangulos e Poligonos Regulares.py
@@ -0,0 +1,6 @@
+#Toda figura geometrica pode ter o numero de diagonais calculadas pela relação: Diagonais = vertices -3. O numero de triangulos será igual ao número de diagonais+1
+
+lados = int(input())
+diagonais = lados - 3
+triangulos = diagonais + 1
+print(triangulos)
